85b8301b2a9ab46779da721408fcebd8ff1b9719
Joost Diepenmaat <joost@zeekat.nl>
2022-06-22 08:36:42 +0200

    Bump versions of deps to support OpenAPI 3.1.0 specification

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject nl.jomco/ring-openapi-validator "0.1.2"
+(defproject nl.jomco/ring-openapi-validator "0.1.3-SNAPSHOT"
   :description "Validate ring requests and responses against Swagger/OpenAPI"
   :url "https://git.sr.ht/~jomco/ring-openapi-validator"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [cheshire "5.10.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [com.atlassian.oai/swagger-request-validator-core "2.18.1"]]
+                 [com.atlassian.oai/swagger-request-validator-core "2.28.1"]
+                 [io.swagger.parser.v3/swagger-parser "2.1.1"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}}
   ;; link to git repo, also ensures that cljdoc.org finds additional markdown files
   :scm {:name "git"
